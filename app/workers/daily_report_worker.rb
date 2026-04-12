3480fa32c764f84cb2e65bf43fa69cbcbfc0f2fa
Northstar <1155211153@link.cuhk.edu.hk>
2026-04-12 21:51:21 +0800

    Refactor daily report worker to use local variables

--- app/workers/daily_report_worker.rb
+++ app/workers/daily_report_worker.rb
@@ -2,11 +2,12 @@ class DailyReportWorker
   include Sidekiq::Job
 
   def perform
-    @new_items = Item.where("created_at >= ?", 24.hours.ago)
+    new_items = Item.where(created_at: 24.hours.ago..Time.current).to_a
 
     User.find_each do |user|
-      @favorite_items = user.favorites
-      UserMailer.daily_report(user, @new_items, @favorite_items).deliver_now
+      favorite_items = user.favorites.to_a
+
+      UserMailer.daily_report(user, new_items, favorite_items).deliver_now
     end
   end
 end
